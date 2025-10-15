0349079e387a89140cdd2cc22f511c57f2e97eaa
Sai Lakshmi <sailakshmi88@gmail.com>
2025-10-15 22:23:05 +0530

    stepsize

--- 02_strings.py
+++ 02_strings.py
@@ -3,3 +3,5 @@ sample_str = "this is a sample string"
 print(sample_str[2])
 #Slicing - if you want to print any sequence of values or ranges of elements from a string
 print(sample_str[0:])
+#stepsize
+print(sample_str[::3])
