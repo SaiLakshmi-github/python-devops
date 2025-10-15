642c0ba145cd382aaf7cdb96ec9bc5f3dcc81c1d
Sai Lakshmi <sailakshmi88@gmail.com>
2025-10-15 21:30:34 +0530

    added datatypes

--- 01_variables.py
+++ 01_variables.py
@@ -25,3 +25,34 @@ print(test_tuple)
 #Dict - stores the values in key value pairs
 test_dict = {'a' : 1, 'b' : 2}
 print(test_dict)
+#set - unordered data, consider and print values in random way
+test_set = {'a',"b",'abc'}
+print(test_set)
+#type () function- is a python buildin function ; prints the datatype of the variable
+print(type(test_dict))
+print(type(print))
+#Operations - Add, Subtract, multiply, divide, integer division, modulo division(reminder)
+a = 42
+b = 6
+c = a+b
+print(a+b)
+d = a-b
+print(d)
+e = a*b
+print(e)
+f = 12
+g = 3
+h = f/g
+print(h)
+print(h, type(h))
+#by default division return float value
+#integer division
+h = f//g
+print(h)
+#reminder(modulo division)
+i = f%g
+print(i)
+#concatenation
+a = "42"
+b = "43"
+print(a+b)
