abcef026b0b262fad58d6c6987a1580b9c79af5d
Sai Lakshmi <sailakshmi88@gmail.com>
2025-10-15 21:18:04 +0530

    datatypes

--- 01_variables.py
+++ 01_variables.py
@@ -1,3 +1,27 @@
 #declaring variable
 a = 42
 print(a)
+#Types of variables
+"""
+-integer
+- Boolean
+ - strings
+  - Dictionary
+"""
+#Datatype
+"""
+-list
+-tuple
+-set
+-Dictionary
+"""
+#Python is case sensite i.e., True and true are 2 different things
+#list
+test_list = ["hello","world","python"]
+print(test_list)
+#tuple
+test_tuple = ("hello","world","python")
+print(test_tuple)
+#Dict - stores the values in key value pairs
+test_dict = {'a' : 1, 'b' : 2}
+print(test_dict)
