7d5f067715c8e466a9752f36f9857a26c1b8caab
Sai Lakshmi <sailakshmi88@gmail.com>
2025-10-15 22:10:22 +0530

    slicing

--- 02_strings.py
+++ 02_strings.py
@@ -1,3 +1,5 @@
 #indexing- prints the placement or position of the element from the string; starts from 0
 sample_str = "this is a sample string"
 print(sample_str[2])
+#Slicing - if you want to print any sequence of values or ranges of elements from a string
+print(sample_str[3:7])
